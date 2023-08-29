b7ebe30b712c4e0e1983ef35e68df9611f9db14f
Light Yagami <lightyagami@lights-mbp.home>
2023-08-28 22:53:46 -0400

    radix works

--- src/radix.c
+++ src/radix.c
@@ -3,14 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   radix.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wmillett <wmillett@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lightyagami <lightyagami@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 22:43:17 by wmillett          #+#    #+#             */
-/*   Updated: 2023/08/27 23:29:58 by wmillett         ###   ########.fr       */
+/*   Updated: 2023/08/28 22:52:43 by lightyagami      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
+
+
+
+
+static int get_max_bits(t_stacks* a) 
+{
+    int max;
+    int max_bits;
+	int i;
+
+	max_bits = 0;
+	max = a->a_stack[0];
+	i = 1;
+	while(i < a->size)
+	{
+		if(a->a_stack[i] > max)
+			max = a->a_stack[i];
+		i++;
+	}
+    while ((max >> max_bits) != 0) 
+        max_bits++;
+    return (max_bits);
+}
 
 static void putback(t_stacks* a)
 {
@@ -24,22 +47,24 @@ static void putback(t_stacks* a)
 static void radix_loop(t_stacks* a)
 {
 	int i;
+	int j;
 	int bit;
 	
-	bit = 1;
-	while(bit < 4)
+	j = 0;
+	bit = get_max_bits(a);
+	while(j < bit)
 	{
 		i = 0;
 		while(i < a->size)
 		{
-			if ((a->a_stack[0] >> bit) & 0)
-				pb(a);
-			else
+			if (((a->a_stack[0] >> j) & 1) == 1)
 				ra(a, FALSE);
+			else
+				pb(a);
 			i++;
 		}
 		putback(a);
-		bit++;
+		j++;
 	}
 }
 
